c3ffdb79c3054b06a3ec93019577c5caf2981c3a
Yashika Gupta <yashikagupta082@gmail.com>
2022-07-17 18:50:04 +0530

    feat:transportListItem added

---
+++ GREEN_COMMUTE/src/components/molecules/transportListItem/index.stories.tsx
@@ -0,0 +1,16 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import ListItem from "./index";
+import ola from "../../../assets/logos/ola.png";
+export default {
+  title: "molecules/TransportListItem",
+  component: ListItem,
+} as ComponentMeta<typeof ListItem>;
+
+const Template: ComponentStory<typeof ListItem> = (args) => <ListItem {...args} />;
+
+export const Main = Template.bind({});
+Main.args = {
+    icon: ola,
+    cname: "Ola",
+    price: "45"
+}
---
+++ GREEN_COMMUTE/src/components/molecules/transportListItem/index.test.tsx
@@ -0,0 +1,10 @@
+import "@testing-library/jest-dom/extend-expect";
+import { render,screen } from "@testing-library/react";
+import ListItem from "./index";
+import ola from "../../../assets/logos/ola.png";
+
+it("renders mui icons", () => {
+  render(<ListItem icon={ola} cname="Ola" price="45" />);
+  const testIcon = screen.getByTestId("listitem");
+  expect(testIcon).toBeInTheDocument();
+});
