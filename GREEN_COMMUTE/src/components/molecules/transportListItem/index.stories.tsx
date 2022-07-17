dcafd8b0ef6bdc597642148a2e0d24e1bb8d50dc
Yashika Gupta <yashikagupta082@gmail.com>
2022-07-17 19:01:03 +0530

    feat:transportListItems added

--- GREEN_COMMUTE/src/components/molecules/transportListItem/index.stories.tsx
+++ GREEN_COMMUTE/src/components/molecules/transportListItem/index.stories.tsx
@@ -1,16 +1,18 @@
-import { ComponentStory, ComponentMeta } from "@storybook/react";
-import ListItem from "./index";
-import ola from "../../../assets/logos/ola.png";
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import ListItem from './index'
+import ola from '../../../assets/logos/ola.svg'
 export default {
-  title: "molecules/TransportListItem",
+  title: 'molecules/TransportListItem',
   component: ListItem,
-} as ComponentMeta<typeof ListItem>;
+} as ComponentMeta<typeof ListItem>
 
-const Template: ComponentStory<typeof ListItem> = (args) => <ListItem {...args} />;
+const Template: ComponentStory<typeof ListItem> = (args) => (
+  <ListItem {...args} />
+)
 
-export const Main = Template.bind({});
+export const Main = Template.bind({})
 Main.args = {
-    icon: ola,
-    cname: "Ola",
-    price: "45"
+  icon: ola,
+  cname: 'Ola',
+  price: '45',
 }
