f91ede117dccc56a194146f22dcd8d185be67e23
Yashika Gupta <yashikagupta082@gmail.com>
2022-07-18 11:05:44 +0530

    fix:themfile modified

--- GREEN_COMMUTE/src/components/molecules/transportListItem/index.tsx
+++ GREEN_COMMUTE/src/components/molecules/transportListItem/index.tsx
@@ -1,54 +1,54 @@
-import Box from "@mui/material/Box";
-import theme from "../../../theme/index";
-import Icon from "../../atoms/Icon";
-import Typography from "@mui/material/Typography";
-import rupee from "../../../assets/icons/rupee.svg";
+import Box from '@mui/material/Box'
+import theme from '../../../theme/theme'
+import Icon from '../../atoms/Icon'
+import Typography from '@mui/material/Typography'
+import rupee from '../../../assets/icons/rupee.svg'
 type ListItemProps = {
-  icon: any;
-  cname: string;
-  price: string;
-};
+  icon: any
+  cname: string
+  price: string
+}
 
 const TransportListItem = (props: ListItemProps) => {
   return (
     <Box
       sx={{
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        width: "359px",
-        minHeight: "60px",
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '359px',
+        minHeight: '60px',
         backgroundColor: `${theme.palette.structural.main}`,
       }}
       data-testid="listitem"
     >
       <Box
         sx={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "center",
-          alignItems: "center",
-          padding: "0px",
-          height: "44px",
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: '0px',
+          height: '44px',
         }}
       >
         <Icon source={props.icon} />
         <Box
           sx={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "flex-start",
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'flex-start',
           }}
         >
           <Box
             sx={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
-              alignItems: "center",
-              margin: "4px 0px",
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+              margin: '4px 0px',
               color: `${theme.palette.textColor.highEmphasis}`,
             }}
           >
@@ -56,11 +56,11 @@ const TransportListItem = (props: ListItemProps) => {
           </Box>
           <Box
             sx={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
-              alignItems: "center",
-              margin: "4px 0px",
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+              margin: '4px 0px',
             }}
           >
             <Typography
@@ -70,7 +70,7 @@ const TransportListItem = (props: ListItemProps) => {
               Approximately
             </Typography>
             <Box
-              sx={{ margin: "0px 8px", display: "flex", alignItems: "center" }}
+              sx={{ margin: '0px 8px', display: 'flex', alignItems: 'center' }}
             >
               <Icon source={rupee} />
               <Typography
@@ -89,14 +89,14 @@ const TransportListItem = (props: ListItemProps) => {
         variant="caption1"
         sx={{
           color: `${theme.palette.primary.primary400}`,
-          display: "flex",
-          alignItems: "center",
+          display: 'flex',
+          alignItems: 'center',
         }}
       >
         Book Now
       </Typography>
     </Box>
-  );
-};
+  )
+}
 
-export default TransportListItem;
+export default TransportListItem
