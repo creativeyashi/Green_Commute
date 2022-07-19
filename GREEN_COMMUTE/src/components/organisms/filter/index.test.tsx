7c2d7ad1c2ab007ed5c59bb3e165c471cb4a177e
saurabh3511 <saurabhsidd42@gmail.com>
2022-07-19 09:34:16 +0530

    create filter pop up

--- GREEN_COMMUTE/src/components/organisms/filter/index.test.tsx
+++ GREEN_COMMUTE/src/components/organisms/filter/index.test.tsx
@@ -1,1 +1,8 @@
 import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import { FilterPopUp } from '.'
+
+it('Text', () => {
+  render(<FilterPopUp />)
+  expect(screen.getByText('Distance')).toBeInTheDocument()
+})
