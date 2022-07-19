4bf24b59a6757ed1db65407b5724f57535684fdc
saurabh3511 <saurabhsidd42@gmail.com>
2022-07-19 15:28:42 +0530

    changes in image folder

--- GREEN_COMMUTE/src/components/atoms/Image/index.stories.tsx
+++ GREEN_COMMUTE/src/components/atoms/Image/index.stories.tsx
@@ -1,29 +1,27 @@
-import { Image, ImageProps} from "./index";
-import { Story } from "@storybook/react";
-import Work from '../../assets/image/Work.png';
-import Stay from '../../assets/image/Entry-image.png';
-import Job from '../../assets/image/Skills.png';
+import { Image, ImageProps } from './index'
+import { Story } from '@storybook/react'
+import Work from '../../../assets/image/Work.png'
+import Stay from '../../../assets/image/Entry-image.png'
+import Job from '../../../assets/image/Skills.png'
 
-export default{
+export default {
+  title: 'Atom/Image',
+  component: Image,
+}
 
-    title: 'Atom/Image',
-    component: Image
+const Template: Story<ImageProps> = (args) => <Image {...args} />
 
-};
+export const WorkComp = Template.bind({})
+WorkComp.args = {
+  source: Work,
+}
 
-const Template : Story<ImageProps> = (args) => <Image {...args} />;
+export const VectorComp = Template.bind({})
+VectorComp.args = {
+  source: Stay,
+}
 
-export const WorkComp = Template.bind({});
-WorkComp.args={
-     source: Work
-};
-
-export const VectorComp = Template.bind({});
-VectorComp.args={
-     source: Stay
-};
-
-export const JobsComp = Template.bind({});
-JobsComp.args={
-     source: Job
-};
+export const JobsComp = Template.bind({})
+JobsComp.args = {
+  source: Job,
+}
