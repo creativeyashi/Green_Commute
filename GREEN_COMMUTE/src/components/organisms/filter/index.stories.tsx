e90609eeaa2e7709e5772ab6e6894544577cff8d
Atif <atifnaeem.mohd@zemosolabs.com>
2022-07-20 20:03:00 +0530

    test cases added for onClear and onApply, checkbox state management done, suggested changes done

--- GREEN_COMMUTE/src/components/organisms/filter/index.stories.tsx
+++ GREEN_COMMUTE/src/components/organisms/filter/index.stories.tsx
@@ -1,12 +1,18 @@
+import { ThemeProvider } from '@emotion/react'
 import { ComponentStory } from '@storybook/react'
-import { FilterPopUp } from '../filter/index'
+import { FilterPopUp } from './index'
+import theme from '../../../theme/theme'
 
 export default {
   title: 'Organisms/filter',
   component: FilterPopUp,
 }
 
-const Template: ComponentStory<typeof FilterPopUp> = () => <FilterPopUp />
+const Template: ComponentStory<typeof FilterPopUp> = () => (
+  <ThemeProvider theme={theme}>
+    <FilterPopUp />
+  </ThemeProvider>
+)
 
 export const FilterTab = Template.bind({})
 FilterTab.args = {}
--- GREEN_COMMUTE/src/components/organisms/filter/index.test.tsx
+++ GREEN_COMMUTE/src/components/organisms/filter/index.test.tsx
@@ -1,8 +1,53 @@
 import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
-import { FilterPopUp } from '.'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { FilterPopUp } from './index'
 
 it('Text', () => {
-  render(<FilterPopUp />)
+  render(
+    <FilterPopUp
+      onApply={(distance: string) => {
+        console.log('Filter Applied' + distance)
+      }}
+      onClear={() => {
+        console.log('cleared')
+      }}
+    />
+  )
   expect(screen.getByText('Distance')).toBeInTheDocument()
+})
+
+it('Clear Button', () => {
+  render(
+    <FilterPopUp
+      onApply={(distance: string) => {
+        console.log('Filter Applied' + distance)
+      }}
+      onClear={() => {
+        console.log('cleared')
+      }}
+    />
+  )
+  const clearButton = screen.getByText('Clear All')
+  expect(clearButton).toBeInTheDocument()
+  fireEvent.click(clearButton, {
+    target: { value: 'JavaScript' },
+  })
+})
+
+it('Apply Button', () => {
+  render(
+    <FilterPopUp
+      onApply={(distance: string) => {
+        console.log('Filter Applied' + distance)
+      }}
+      onClear={() => {
+        console.log('cleared')
+      }}
+    />
+  )
+  const applyButton = screen.getByText('Apply')
+  expect(applyButton).toBeInTheDocument()
+  fireEvent.click(applyButton, {
+    target: { value: 'JavaScript' },
+  })
 })
