b814168f9e63b3b31706134bf890cd063ed9d5cb
Atif <atifnaeem.mohd@zemosolabs.com>
2022-07-20 14:59:53 +0530

    passing demo props

--- GREEN_COMMUTE/src/components/templates/homeScreen/Index.stories.tsx
+++ GREEN_COMMUTE/src/components/templates/homeScreen/Index.stories.tsx
@@ -1,3 +1,4 @@
+import Typography from '@mui/material/Typography'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 import HomeScreenTemplate from './Index'
 
@@ -10,5 +11,10 @@ const Template: ComponentStory<typeof HomeScreenTemplate> = (args) => (
   <HomeScreenTemplate {...args} />
 )
 
-export const LandingTemplate = Template.bind({})
-LandingTemplate.args = {}
+export const HomeScreen = Template.bind({})
+HomeScreen.args = {
+  Header: <Typography variant="h6">Header</Typography>,
+
+  SideMenu: <Typography variant="h6">Side Menu</Typography>,
+  MainContent: <Typography variant="h6">Main Content</Typography>,
+}
