304916e3cd67d313816bba4f7649d31444c8444c
Atif <atifnaeem.mohd@zemosolabs.com>
2022-07-21 13:55:32 +0530

    refactoring done

--- GREEN_COMMUTE/src/components/organisms/filter/index.tsx
+++ GREEN_COMMUTE/src/components/organisms/filter/index.tsx
@@ -26,6 +26,111 @@ const initialState = {
   far: false,
 }
 
+const distanceData = [
+  {
+    name: 'closest',
+    label: '0 - 10 Kms',
+    value: true,
+  },
+  {
+    name: 'nearest',
+    label: '10 - 20 Kms',
+    value: false,
+  },
+  {
+    name: 'intermediate',
+    label: '21 - 30 Kms',
+    value: false,
+  },
+  {
+    name: 'far',
+    label: '31 - 40 Kms',
+    value: false,
+  },
+]
+const datePosted = [
+  {
+    name: 'Past 24 hours',
+    checked: true,
+  },
+  {
+    name: 'Past week',
+    checked: false,
+  },
+  {
+    name: 'Past month',
+    checked: false,
+  },
+  {
+    name: 'Anytime',
+    checked: false,
+  },
+]
+const greenCommute = [
+  {
+    name: 'Yes',
+    checked: true,
+  },
+  {
+    name: 'Past week',
+    checked: false,
+  },
+]
+const jobType = [
+  {
+    name: 'Full time',
+    checked: true,
+  },
+  {
+    name: 'Internship',
+    checked: false,
+  },
+  {
+    name: 'Contract',
+    checked: false,
+  },
+  {
+    name: 'Remote',
+    checked: false,
+  },
+]
+const experienceLevel = [
+  {
+    name: 'Fresher',
+    checked: true,
+  },
+  {
+    name: 'Mid-level',
+    checked: false,
+  },
+  {
+    name: 'Director',
+    checked: false,
+  },
+  {
+    name: 'Executive',
+    checked: false,
+  },
+]
+const transport = [
+  {
+    name: 'Metro',
+    checked: true,
+  },
+  {
+    name: 'Bus',
+    checked: true,
+  },
+  {
+    name: 'Car pooling',
+    checked: true,
+  },
+  {
+    name: 'Motor Cycle',
+    checked: true,
+  },
+]
+
 const useStyles = makeStyles({
   MainContainer: {
     border: '1px solid green',
@@ -103,94 +208,63 @@ export const FilterPopUp = ({ onApply, onClear }: Props) => {
           <div className={style.check}>
             <Typography variant="body1">Distance</Typography>
           </div>
-          <div className={style.check}>
-            <CheckBoxComponent
-              checked={closest}
-              onChange={handleChange}
-              name="closest"
-            />
-            <Typography variant="body2" className={style.typography}>
-              0 - 10 Kms
-            </Typography>
-          </div>
-          <div className={style.check}>
-            <CheckBoxComponent
-              checked={nearest}
-              onChange={handleChange}
-              name="nearest"
-            />
-            <Typography variant="body2" className={style.typography}>
-              11 - 20 Kms
-            </Typography>
-          </div>
-          <div className={style.check}>
-            <CheckBoxComponent
-              checked={intermediate}
-              onChange={handleChange}
-              name="intermediate"
-            />
-            <Typography variant="body2" className={style.typography}>
-              21 - 30 Kms
-            </Typography>
-          </div>
-          <div className={style.check}>
-            <CheckBoxComponent
-              checked={far}
-              onChange={handleChange}
-              name="far"
-            />
-            <Typography variant="body2" className={style.typography}>
-              31 - 40 Kms
-            </Typography>
-          </div>
+          {distanceData.map((element, index) => {
+            switch (element.name) {
+              case 'closest':
+                element.value = closest
+                break
+              case 'nearest':
+                element.value = nearest
+                break
+              case 'intermediate':
+                element.value = intermediate
+                break
+              case 'far':
+                element.value = far
+            }
+            return (
+              <div className={style.check} key={index}>
+                <CheckBoxComponent
+                  checked={element.value}
+                  onChange={handleChange}
+                  name={element.name}
+                />
+                <Typography variant="body2" className={style.typography}>
+                  {element.label}
+                </Typography>
+              </div>
+            )
+          })}
         </Box>
-
         <Box>
           <div className={style.check}>
             <Typography>Date Posted</Typography>
           </div>
-          <div className={style.check}>
-            <CheckBoxComponent checked={true} />
-            <Typography variant="body2" className={style.typography}>
-              Past 24 hours
-            </Typography>
-          </div>
-          <div className={style.check}>
-            <CheckBoxComponent checked={false} />
-            <Typography variant="body2" className={style.typography}>
-              Past week
-            </Typography>
-          </div>
-          <div className={style.check}>
-            <CheckBoxComponent checked={false} />
-            <Typography variant="body2" className={style.typography}>
-              Past month
-            </Typography>
-          </div>
-          <div className={style.check}>
-            <CheckBoxComponent checked={false} />
-            <Typography variant="body2" className={style.typography}>
-              Anytime
-            </Typography>
-          </div>
+          {datePosted.map((element, index) => {
+            return (
+              <div className={style.check} key={index}>
+                <CheckBoxComponent checked={element.checked} />
+                <Typography variant="body2" className={style.typography}>
+                  {element.name}
+                </Typography>
+              </div>
+            )
+          })}
         </Box>
-
         <Box>
           <div className={style.check}>
             <Typography>Green Commute</Typography>
           </div>
-          <div className={style.check}>
-            <RadioButton checked={true} />
-            <Typography variant="body2" className={style.typography}>
-              Yes
-            </Typography>
-          </div>
-          <div className={style.check}>
-            <RadioButton checked={false} />
-            <Typography variant="body2" className={style.typography}>
-              Past week
-            </Typography>
-          </div>
+          {greenCommute.map((element, index) => {
+            return (
+              <div className={style.check} key={index}>
+                <RadioButton checked={element.checked} />
+                <Typography variant="body2" className={style.typography}>
+                  {element.name}
+                </Typography>
+              </div>
+            )
+          })}
         </Box>
       </Box>
 
@@ -199,89 +273,47 @@ export const FilterPopUp = ({ onApply, onClear }: Props) => {
           <div className={style.check}>
             <Typography>Job Type</Typography>
           </div>
-          <div className={style.check}>
-            <CheckBoxComponent checked={true} />
-            <Typography variant="body2" className={style.typography}>
-              Full time
-            </Typography>
-          </div>
-          <div className={style.check}>
-            <CheckBoxComponent checked={false} />
-            <Typography variant="body2" className={style.typography}>
-              Internship
-            </Typography>
-          </div>
-          <div className={style.check}>
-            <CheckBoxComponent checked={false} />
-            <Typography variant="body2" className={style.typography}>
-              Contract
-            </Typography>
-          </div>
-          <div className={style.check}>
-            <CheckBoxComponent checked={false} />
-            <Typography variant="body2" className={style.typography}>
-              Remote
-            </Typography>
-          </div>
+          {jobType.map((element, index) => {
+            return (
+              <div className={style.check} key={index}>
+                <CheckBoxComponent checked={element.checked} />
+                <Typography variant="body2" className={style.typography}>
+                  {element.name}
+                </Typography>
+              </div>
+            )
+          })}
         </Box>
         <Box>
           <div className={style.check}>
             <Typography>Experience Level</Typography>
           </div>
-          <div className={style.check}>
-            <CheckBoxComponent checked={true} />
-            <Typography variant="body2" className={style.typography}>
-              Fresher
-            </Typography>
-          </div>
-          <div className={style.check}>
-            <CheckBoxComponent checked={false} />
-            <Typography variant="body2" className={style.typography}>
-              Mid-level
-            </Typography>
-          </div>
-          <div className={style.check}>
-            <CheckBoxComponent checked={false} />
-            <Typography variant="body2" className={style.typography}>
-              Director
-            </Typography>
-          </div>
-          <div className={style.check}>
-            <CheckBoxComponent checked={false} />
-            <Typography variant="body2" className={style.typography}>
-              Executive
-            </Typography>
-          </div>
+          {experienceLevel.map((element, index) => {
+            return (
+              <div className={style.check} key={index}>
+                <CheckBoxComponent checked={element.checked} />
+                <Typography variant="body2" className={style.typography}>
+                  {element.name}
+                </Typography>
+              </div>
+            )
+          })}
         </Box>
 
         <Box>
           <div className={style.check}>
             <Typography>Transport</Typography>
           </div>
-          <div className={style.check}>
-            <CheckBoxComponent checked={true} />
-            <Typography variant="body2" className={style.typography}>
-              metro
-            </Typography>
-          </div>
-          <div className={style.check}>
-            <CheckBoxComponent checked={true} />
-            <Typography variant="body2" className={style.typography}>
-              Bus
-            </Typography>
-          </div>
-          <div className={style.check}>
-            <CheckBoxComponent checked={true} />
-            <Typography variant="body2" className={style.typography}>
-              Car pooling
-            </Typography>
-          </div>
-          <div className={style.check}>
-            <CheckBoxComponent checked={true} />
-            <Typography variant="body2" className={style.typography}>
-              Motor Cycle
-            </Typography>
-          </div>
+          {transport.map((element, index) => {
+            return (
+              <div className={style.check} key={index}>
+                <CheckBoxComponent checked={element.checked} />
+                <Typography variant="body2" className={style.typography}>
+                  {element.name}
+                </Typography>
+              </div>
+            )
+          })}
         </Box>
       </Box>
       <div className={style.footer}>
