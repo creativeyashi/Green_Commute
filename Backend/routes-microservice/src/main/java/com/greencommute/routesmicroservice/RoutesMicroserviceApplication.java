96c230244eb563877c994cbcd3ac922760f046e2
Yashika Gupta <yashikagupta082@gmail.com>
2022-08-07 04:25:50 +0530

    feat:sonar-setup-route-service

---
+++ Backend/routes-microservice/src/main/java/com/greencommute/routesmicroservice/RoutesMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.greencommute.routesmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutesMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutesMicroserviceApplication.class, args);
+	}
+
+}
---
+++ Backend/routes-microservice/src/test/java/com/greencommute/routesmicroservice/RoutesMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greencommute.routesmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutesMicroserviceApplicationTests {
+
+	// @Test
+	// void contextLoads() {
+	// }
+
+}
