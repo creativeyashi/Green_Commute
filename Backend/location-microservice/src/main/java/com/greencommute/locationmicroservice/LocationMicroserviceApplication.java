e9526cff3e47c686b4d2baa39e063b2b75174f95
Yashika Gupta <yashikagupta082@gmail.com>
2022-08-07 03:56:51 +0530

    feat:sonar-setup-location-service

---
+++ Backend/location-microservice/src/main/java/com/greencommute/locationmicroservice/LocationMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.greencommute.locationmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationMicroserviceApplication.class, args);
+	}
+
+}
---
+++ Backend/location-microservice/src/test/java/com/greencommute/locationmicroservice/LocationMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greencommute.locationmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class LocationMicroserviceApplicationTests {
+
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+// }
