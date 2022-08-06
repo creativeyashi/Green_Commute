1ab2d6506185bc04b82892d77800129f6367bf36
Yashika Gupta <yashikagupta082@gmail.com>
2022-08-07 04:10:48 +0530

    feat:sonar-setup-jobs-microservice

---
+++ Backend/job-microservice/src/main/java/com/greencommute/jobmicroservice/JobMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.greencommute.jobmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobMicroserviceApplication.class, args);
+	}
+
+}
---
+++ Backend/job-microservice/src/test/java/com/greencommute/jobmicroservice/JobMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greencommute.jobmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
