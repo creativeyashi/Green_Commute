4bd5b7c79b4c0a2ed5bda44d0f69d03d6c6a5f5a
prakhar-zemoso <prakhar.pandey@zemosolabs.com>
2022-08-16 16:36:37 +0530

    backendFinished

--- Backend/routes-microservice/src/main/java/com/greencommute/routesmicroservice/routes/controller/RouteController.java
+++ Backend/routes-microservice/src/main/java/com/greencommute/routesmicroservice/routes/controller/RouteController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/routes")
+@CrossOrigin
 public class RouteController {
 
     @Autowired
--- Backend/skills-microservice/src/main/java/com/greencommute/skillsmicroservice/controller/SkillController.java
+++ Backend/skills-microservice/src/main/java/com/greencommute/skillsmicroservice/controller/SkillController.java
@@ -7,6 +7,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/skills")
+@CrossOrigin
 @Log4j2
 public class SkillController {
 
